d06f511af343369ef21bc3f892f87afe80aa494a
puri <purip586@gmail.com>
2024-01-22 23:26:53 +0900

    cc

--- next.config.js
+++ next.config.js
@@ -8,9 +8,9 @@ const nextConfig = {
       remotePatterns: [
         {
           protocol: 'https',
-          hostname: '',
+          hostname: 'cloud.mongodb.com',
           port: '',
-          pathname: '',
+          pathname: '/account123/**',
         },
       ],
     },
